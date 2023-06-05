a59b3bcf7ce9ee0ee9522cc1cd157e33d1115c36
rocoide rocoide <roodrigo4@hotmail.com>
2023-06-05 17:07:36 -0300

    grafico plot y histograma utiles

--- tp.py
+++ tp.py
@@ -21,23 +21,28 @@ data_base = pd.read_csv(URL)
 data_base['FECHA'] = pd.to_datetime(data_base['FECHA'], format='%d%b%Y:%H:%M:%S')
                                                     # cambio los datos de la columna fecha a datos tipo datetime
 
-data_2022 = data_base[data_base['FECHA'].dt.year == 2022]
+data_2022 = data_base[(data_base['FECHA'].dt.year <= 2022) & (data_base['FECHA'].dt.year >= 2019)]
                                                     # creo una base de datos solo con los valores del año 2022
 
 data_2022['FECHA'] = pd.to_datetime(data_2022['FECHA'], format='%d%m%Y:%H:%M:%S')
                                                     # cambio los datos de la columna fecha a datos tipo datetime
 
-#data_2022 = data_2022.sort_values("FECHA")
+data_2022 = data_2022.sort_values("FECHA")
 
 data_2022 = data_2022.astype(str)
 data_2022 = data_2022[data_2022['CO_CENTENARIO'] != 'nan']
 data_2022 = data_2022[data_2022['CO_CENTENARIO'] != 's/d']
-#data_2022 = data_2022.sort_values("CO_CENTENARIO", ascending=True)
-# data_2022 = data_2022[data_2022['CO_CORDOBA'] != 'nan']
-# data_2022 = data_2022[data_2022['CO_CORDOBA'] != 's/d']
+data_2022 = data_2022[data_2022['CO_CORDOBA'] != 'nan']
+data_2022 = data_2022[data_2022['CO_CORDOBA'] != 's/d']
+data_2022 = data_2022[data_2022['CO_LA_BOCA'] != 'nan']
+data_2022 = data_2022[data_2022['CO_LA_BOCA'] != 's/d']
+data_2022 = data_2022[data_2022['CO_LA_BOCA'] != '#REF!']
+#data_2022 = data_2022[data_2022['CO_PALERMO'] != 'nan']
+#data_2022 = data_2022[data_2022['CO_PALERMO'] != 's/d']
                                                     #filtro las filas de las base de datos donde CO centenario tenga un campo vacio o un s/d
 
 data_2022 = data_2022.drop_duplicates(subset='FECHA', keep='last')
+print (data_2022)
 
 #print(data_2022)
 #                                                     #elimino las fechas repetidas, esto se podria mejorar calculando la media del dia
@@ -51,36 +56,69 @@ data_2022 = data_2022.drop_duplicates(subset='FECHA', keep='last')
 
 
 
-co_centenario = data_2022.CO_CENTENARIO
-                                                    #Histograma
-# #
-# #
-# #
-fig2 = plt.figure(figsize=(9, 5))
-# #                                                     # Tamaño de la figura
-# # #niveles = [0, 0.20, 0.40, 0.60, 0.80, 1, 1.20, 1.40, 1.60, 1.80, 2, 2.20, 2.40]
-plt.hist(co_centenario, ec="red")
-# #                                                     # la libreria plt tiene a ".hist" que genera un histograma
-# #                                                     # de la columna guardada en la variable "atributo".
-# #                                                     # bins: número de intervalos en los que se divide el rango.
-# # plt.xlabel('Niveles de monoxido de carbono -CO')
-# # #plt.xlim(0, 2.4)  # Establecer límites del eje x
-# # #plt.xticks(niveles)  # Establecer las marcas en el eje x
 
-# # plt.ylabel('Cantidad de dias en el año')
-# #                                                     # etiqueta eje y
-# # fig2.suptitle('Histograma del monoxido de carbono en la estacion Centenario')
-# # #título
-# # #plt.legend(bbox_to_anchor=(1.0, 1), loc='upper left') # posición del título
+
+
+
+
+data_2022.CO_CENTENARIO = data_2022.CO_CENTENARIO.astype(float)
+co_centenario = data_2022.CO_CENTENARIO
+                                                     #Histograma
+
+
+fig2 = plt.figure(figsize=(9, 5))
+                                                     # Tamaño de la figura
+niveles = [0, 0.20, 0.40, 0.60, 0.80, 1, 1.20, 1.40, 1.60, 1.80, 2, 2.20, 2.40]
+plt.hist(co_centenario, bins=niveles, ec="red")
+                                                    # la libreria plt tiene a ".hist" que genera un histograma
+                                                    # de la columna guardada en la variable "atributo".
+                                                    # bins: número de intervalos en los que se divide el rango.
+plt.xlabel('Niveles de monoxido de carbono -CO')
+#plt.xticks(niveles)  
+                                                    # Establecer las marcas en el eje x
+
+plt.ylabel('Cantidad de dias')
+                                                    # etiqueta eje y
+fig2.suptitle('Histograma del monoxido de carbono en la estacion Centenario', fontsize=14, fontweight="bold")
+
+
+mode=sc.stats.mode(co_centenario, keepdims=False)[0]                                                    #título
+plt.axvline(x = co_centenario.mean(), color = 'black', label = 'valor medio de {} = {} '.format(co_centenario.name, round(co_centenario.mean())))
+plt.axvline(x = co_centenario.median(), color = 'red', label = 'mediana de {} = {} '.format(co_centenario.name, round(co_centenario.median())))
+plt.axvline(x = mode, color = 'pink', label = 'moda de {} = {} '.format(co_centenario.name, mode))
+plt.legend(bbox_to_anchor=(1.0, 1), loc='upper right')
+
+
 plt.show()
 
 
 
-#                                                     #Grafico plot
-#plt.plot(data_2022.FECHA[::5], data_2022.CO_CENTENARIO[::5])
-# plt.plot(data_2022.FECHA[::7], data_2022.CO_CORDOBA[::7])
-# # # plt.xlabel("Fecha")
-# # # plt.ylabel("Monóxido de Carbono")
-# # # plt.title("Niveles de Monóxido de Carbono en 2022")
-#plt.xticks(rotation=45)
-#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+# # # #                                                     #Grafico plot
+# plt.title("Niveles de Monóxido de Carbono de 2021 a 2022", fontdict={"fontweight":"bold", "fontsize":22})
+
+# data_2022['FECHA'] = pd.to_datetime(data_2022['FECHA'])
+# data_2022 = data_2022.sort_values("FECHA")
+
+# data_2022.CO_CENTENARIO = data_2022.CO_CENTENARIO.astype(float)
+# data_2022.CO_CORDOBA = data_2022.CO_CORDOBA.astype(float)
+# data_2022.CO_LA_BOCA = data_2022.CO_LA_BOCA.astype(float)
+
+# plt.plot(data_2022.FECHA, data_2022.CO_CENTENARIO, 'y.-', label="centenario")
+# plt.plot(data_2022.FECHA, data_2022.CO_CORDOBA, 'r.-', label="cordoba")
+# plt.plot(data_2022.FECHA, data_2022.CO_LA_BOCA, 'b.-', label="La Boca")
+# plt.xlabel("Fechas")
+# plt.ylabel("Nivel de Monóxido de Carbono")
+# # plt.xticks(rotation=45)
+# plt.legend()
+# plt.show()
